0d9105894fa46dca506db3c586f2229600555a21
Enzo Courvalet <courvaletenzo@gmail.com>
2026-02-21 19:19:00 +0100

    fix bug

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,8 @@ export default function Contact() {
       id="contact"
       className="relative h-screen flex flex-wrap justify-center items-center bg-linear-to-r from-blue-900 to-violet-900"
     >
+
+    <MatrixBackground />
       <div className="relative z-10 flex flex-col items-center w-full">
         <h2 className="text-3xl font-bold text-white mb-6 text-center">
           Contact
