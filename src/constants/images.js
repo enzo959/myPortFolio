fc2b181cdc73982d5a177f36ce68f0db300a8be6
Enzo Courvalet <courvaletenzo@gmail.com>
2026-04-30 11:40:18 +0200

    update image skills

--- src/constants/images.js
+++ src/constants/images.js
@@ -11,6 +11,15 @@ import tailwind from "../assets/tailwind.png";
 import virtualbox from "../assets/virtualbox.png";
 import js from "../assets/js.png"
 import linux from "../assets/linux.png"
+import python from "..assets/python.png"
+import openStego from "..assets/openStego.png"
+import autopsy from "..assets/autopsy.png"
+import veraScrypt from "..assets/veraScrypt.png"
+import typScipt from "..assets/typeScript.png"
+
+
+
+
 
 
 export const images = {
@@ -26,6 +35,11 @@ export const images = {
   tailwind,
   virtualbox,
   js,
-  linux
+  linux,
+  python,
+  openStego,
+  autopsy,
+  veraScrypt,
+  typScipt
 
 };
